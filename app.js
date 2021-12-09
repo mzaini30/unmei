e7e28d7736b6afdf2df96c738814f7ec0b5db2ec
mzaini30 <muhzaini30@gmail.com>
2021-12-10 00:59:02 +0800

    Sudah sementara

--- app.js
+++ app.js
@@ -2,43 +2,99 @@
 
 import express from 'express'
 import nunjucks from 'nunjucks'
-
-import { execSync } from 'child_process'
+import { resolve, basename, dirname } from 'path'
+import mkdirp from 'mkdirp'
+import { readFileSync, writeFileSync } from 'fs'
+import chalk from 'chalk'
+import glob from 'glob'
+import chokidar from 'chokidar'
+// import { exec } from 'child_process'
 
 if (process.argv.includes('build')) {
   // generate nunjucks
-  execSync('npx nunjucks "**/*.html" -p src -o build')
-}
+  // exec('nunjucks "**/*.html" -p src -o build', () => {})
 
-if (process.argv.includes('server')) {
-  const app = express()
+  const nunjucksEnv = nunjucks.configure('src', {
+    trimBlocks: true,
+    lstripBlocks: true,
+    noCache: true
+  })
+  const outputDir = 'build'
 
-  // Define port to run server on
-  var port = process.env.PORT || 3000;
+  // const context = argv._[1] ? JSON.parse(readFileSync(argv._[1], 'utf8')) : {}
+  const context = {}
+  // Expose environment variables to render context
+  // context.env = process.env
 
-  // Configure Nunjucks
-  var _templates = process.env.NODE_PATH ? process.env.NODE_PATH + '/src' : 'src';
-  nunjucks.configure(_templates, {
-    autoescape: true,
-    cache: false,
-    express: app
-  });
+  function render(files) {
+    for (const file of files) {
+      // No performance benefits in async rendering
+      // https://mozilla.github.io/nunjucks/api.html#asynchronous-support
+      const res = nunjucksEnv.render(file, context)
 
-  // Set Nunjucks as rendering engine for pages with .html suffix
-  app.engine('html', nunjucks.render);
-  app.set('view engine', 'html');
+      let outputFile = file.replace(/\.\w+$/, `.html`)
 
-  app.get('/favicon.ico', (req, res) => res.status(204));
-  // Respond to all GET requests by rendering relevant page using Nunjucks
-  app.get(/\/(.+)/, function(req, res) {
-    res.render(req.params[0]);
-  });
+      if (outputDir) {
+        outputFile = resolve(outputDir, outputFile)
+        mkdirp.sync(dirname(outputFile))
+      }
 
-  // Start server
-  app.listen(port);
-  console.log('Listening on port %s...', port);
+      console.log(chalk.blue('Rendering: ' + file))
+      writeFileSync(outputFile, res)
+    }
+  }
+
+  const globOptions = { strict: true, cwd: 'src', ignore: '**/_*.*', nonull: true }
+
+  // Render the files given a glob pattern (except the ones starting with "_")
+  glob('**/*.html', globOptions, (err, files) => {
+    if (err) return console.error(chalk.red(err))
+    render(files)
+  })
 }
 
 if (process.argv.includes('dev')) {
-  execSync('npx nodemon -e html --exec "npx unmei server"')
+  const app = express()
+
+  function server() {
+
+    // Define port to run server on
+    
+
+    // Configure Nunjucks
+    var _templates = process.env.NODE_PATH ? process.env.NODE_PATH + '/src' : 'src';
+    nunjucks.configure(_templates, {
+      autoescape: true,
+      cache: false,
+      express: app
+    });
+
+    // Set Nunjucks as rendering engine for pages with .html suffix
+    app.engine('html', nunjucks.render);
+    app.set('view engine', 'html');
+
+    app.get('/favicon.ico', (req, res) => res.status(204));
+    // Respond to all GET requests by rendering relevant page using Nunjucks
+    app.get(/\/(.+)/, function(req, res) {
+      res.render(req.params[0]);
+    });
+
+  }
+  const port = process.env.PORT || 3000;
+  // Start server
+  app.listen(port);
+  console.log('Listening on port %s...', port);
+
+  chokidar.watch('./src', {
+    // ignored: './dev/static/windi.css',
+    // awaitWriteFinish: {
+    //   stabilityThreshold: 500
+    // }
+  }).on('all', (event, path) => {
+    server()
+  });
 }
+
+// if (process.argv.includes('dev')) {
+//   exec('nodemon -e html --exec "unmei server"', () => {})
+// }
